c6f1d03e7551efcaad13056250bc1186b8626572
Henning Liebenau <henning.liebenau@protonmail.com>
2023-10-05 21:31:57 +0200

    tools/complete.asm: Added comments, minor cleanup

--- tools/complete.asm
+++ tools/complete.asm
@@ -1,8 +1,17 @@
+; this file is only needed as a basis to
+; run the verification targets and the
+; dasm-export target
+
 *=0x1000
-wic64_include_load_and_run = 1
+
 wic64_include_return_to_portal = 1
 !src <wic64.h>
 !src <wic64.asm>
+
+; these symbols must be mentionsne so that they
+; are defined in the symbolfile and can be
+; included in the dasm-export by export.rb
+
 jsr wic64_execute
 jsr wic64_detect
 jsr wic64_return_to_portal
